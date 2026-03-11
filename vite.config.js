eb36828799611ebf1070ae48809cd135eecd750d
Claudine <claudine@likeavirgin.be>
2026-03-11 17:23:44 +0100

    added option to switch check list

--- vite.config.js
+++ vite.config.js
@@ -2,35 +2,35 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig(({ command }) => ({
-  base: command === "serve" ? "" : "/dist/",
-  build: {
-    emptyOutDir: true,
-    manifest: true,
-    outDir: "./web/dist",
-    rollupOptions: {
-      input: {
-        app: "./src/scripts/main.js",
-        css: "./src/styles/main.css",
-      },
-      output: {
-        sourcemap: true,
-      },
+    base: command === "serve" ? "" : "/dist/",
+    build: {
+        emptyOutDir: true,
+        manifest: true,
+        outDir: "./web/dist",
+        rollupOptions: {
+            input: {
+                app: "./src/scripts/main.js",
+                css: "./src/styles/main.css",
+            },
+            output: {
+                sourcemap: true,
+            },
+        },
     },
-  },
-  publicDir: false,
-  server: {
-    origin: "http://localhost:5173",
-    cors: true,
-    hmr: {
-      host: "localhost",
-      protocol: "ws",
+    publicDir: false,
+    server: {
+        origin: "http://localhost:5173",
+        cors: true,
+        hmr: {
+            host: "localhost",
+            protocol: "ws",
+        },
+        proxy: {
+            "/assets/fonts": {
+                target: "https://vydraulics.ddev.site",
+                changeOrigin: true,
+                secure: false,
+            },
+        },
     },
-    proxy: {
-      "/assets/fonts": {
-        target: "https://vydraulics.ddev.site",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 }));

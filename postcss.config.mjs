f5304bec888a610b2bcdb9ce099840ce85e33b4e
Claudine <claudine@likeavirgin.be>
2026-03-12 09:16:25 +0100

    removed comment

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,7 +2,7 @@ import postcssNested from "postcss-nested";
 
 export default {
   plugins: {
-    "@tailwindcss/postcss": {},
     "postcss-nested": postcssNested,
+    "@tailwindcss/postcss": {},
   },
 };
